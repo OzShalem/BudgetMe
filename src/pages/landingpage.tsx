30949b31a596536a3dbdc74fc7f416bd76ffa9e3
Oz Shalem <ozshalem98@gmail.com>
2025-01-16 15:59:15 +0100

    small fix

--- src/pages/landingpage.tsx
+++ src/pages/landingpage.tsx
@@ -1,4 +1,4 @@
-import FAQ from '../components/faq.tsx'
+import FAQ from '../components/faq'
 import Features from '../components/features'
 import Hero from '../components/hero'
 import LandingCF from '../components/landingcf'
